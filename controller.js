e6c254aa139cb253eaf9a9f99c5a9db44612f88e
Cauehcs <hcs.caue@gmail.com>
2021-07-06 00:09:19 -0300

    Header: Animação do header adicionada

--- controller.js
+++ controller.js
@@ -2,10 +2,13 @@ let currentTabHeader = 0;
 
 function toTab(index) {
   if(document.getElementById('active-tab-header') !== null) {
-    document.getElementById('active-tab-header').id = '';
+    document.getElementById('active-tab-header').id = 'inactive-tab-header';
     document.getElementById('active-tab-content').id = '';
   }
   currentTabHeader = index;
   document.getElementsByClassName('box-header-child')[index].id = 'active-tab-header';
   document.getElementsByClassName('box-content-child')[index].id = 'active-tab-content';
+  setTimeout(() => {
+    document.getElementById('inactive-tab-header').id = '';
+  }, 500);
 }
